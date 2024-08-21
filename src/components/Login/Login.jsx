b294af03b9abad11ed48f4cb325b9bf8ab02e567
i2r4y <estefhanyrayane81@gmail.com>
2024-08-21 17:17:56 -0300

    Login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,41 @@
 import React from "react";
+import "./Login.css";
+import { assets } from "../../assets/assets";
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="login">
+      <div className="escrita">
+        <img src="" alt="" />
+
+        <h3>FestCentral</h3>
+
+        <p>
+          Descubra eventos <br />
+          personalizados. <br />
+          Inscreva-se para receber
+          <br />
+          recomendações personalizadas hoje mesmo!
+        </p>
+      </div>
+
+      <div className="login-filho">
+        <div className="login-container">
+          <div className="login-style">
+            <h3>Login</h3>
+          </div>
+          <div className="login-logado">
+            <div className="rede-social">
+              <img src={assets.google_play} alt="" />
+              <span>Faça login com o Google</span>
+            </div>
+            <div className="rede-social">
+              <img src={assets.facebook_icon} alt="" />
+              <span>Entrar com o Facebook</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
